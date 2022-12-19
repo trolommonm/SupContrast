a0998f646a5498d571b328b2b3fd65dc91751618
Alvin Tan De Jun <>
2022-12-19 11:35:42 +0800

    comment tensorboard_logger import

--- main_supcon.py
+++ main_supcon.py
@@ -6,7 +6,7 @@ import argparse
 import time
 import math
 
-import tensorboard_logger as tb_logger
+# import tensorboard_logger as tb_logger
 from torch.utils.tensorboard import SummaryWriter
 import torch
 import torch.backends.cudnn as cudnn
